c1ac625d96f217415b6a7e50762be186e7852881
izc05 <199996336+izc05@users.noreply.github.com>
2026-04-22 07:14:07 +0200

    fix pages publishing with production bundle

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: fileURLToPath(new URL('./site', import.meta.url)),
   base: './',
-  plugins: [react()]
+  publicDir: fileURLToPath(new URL('./public', import.meta.url)),
+  plugins: [react()],
+  build: {
+    outDir: fileURLToPath(new URL('./dist', import.meta.url)),
+    emptyOutDir: true
+  }
 })
